eb4524ac77a302e8c070bb0c79fcf750d6e43e5a
Marc Hu <marc.hu@outlook.fr>
2017-04-28 21:17:49 +0200

    ajout menu pantalon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("Ce programme JS vient d'être chargé");
 $(document).ready(function() {
-    $('#ourlet').hide();
+    $('.plusMoins').hide();
     console.log("Le document est pret");
     $('#formClient input').keyup(function(e) {
         console.log("touche appuyé");
@@ -14,6 +14,12 @@ $(document).ready(function() {
 
     $('#boutonOK').mousedown(function(e) {
         console.log("Bouton OK cliqué");
+        var nom = $('#nomP').val();
+        console.log(nom);
+        if (nom != null) {
+            alert("Erreur! Vous avez déjà saisie un client!");
+            return;
+        }
         if ($('#formClient span').is(":visible"))
             alert("Erreur! Certains champs ne sont pas complétés ou incorrect.");
         else {
@@ -22,24 +28,29 @@ $(document).ready(function() {
                 prenom: $('#prenom').val(),
                 date: $('#date').val()
             };
-            var nom = $('<tr><td class="nomP"></td></tr><tr><td class="prenomP"></td></tr><tr><td class="dateP"></td></tr>');
-            nom.find('.nomP').text("Nom du client : " + client.nom.toUpperCase());
-            nom.find('.prenomP').text("Prénom du client : " + client.prenom.toUpperCase());
-            nom.find('.dateP').text("Date de dépôt : " + client.date);
+            var nom = $('<tr><td id="nomP"></td></tr><tr><td id="prenomP"></td></tr><tr><td id="dateP"></td></tr>');
+            nom.find('#nomP').text("Nom du client : " + client.nom.toUpperCase());
+            nom.find('#prenomP').text("Prénom du client : " + client.prenom.toUpperCase());
+            nom.find('#dateP').text("Date de dépôt : " + client.date);
             $('#recap').append(nom);
         }
     });
 
-    $('#boutonOurlet').click(function(e) {
+    $('.boutonDev').click(function(e) {
         console.log("Bouton ourlet cliqué");
-        if ($('#boutonOurlet').text().localeCompare("+ Ourlet") === 0) {
+        var nom = $('#nomP').val();
+        if (nom == null) {
+            alert("Erreur! Vous n'avez pas encore saisie de client.");
+            return;
+        }
+        if ($(this).text().charAt(0) == '+') {
             console.log("Clique bouton plus");
-            $('#ourlet').show();
-            $('#boutonOurlet').text($('#boutonOurlet').text().replace('+ Ourlet', '- Ourlet'));
+            $(this).parent().find($('.plusMoins')).first().show();
+            $(this).text($(this).text().replace('+', '-'));
         } else {
             console.log("Clique bouton moins");
-            $('#ourlet').hide();
-            $('#boutonOurlet').text($('#boutonOurlet').text().replace('- Ourlet', '+ Ourlet'));
+            $(this).parent().find($('.plusMoins')).first().hide();
+            $(this).text($(this).text().replace('-', '+'));
         }
     });
 

9aba2c7fbc4e3a14c6147c5f08a15aecfcb98441
Marc Hu <marc.hu@outlook.fr>
2017-04-28 19:13:09 +0200

    Ajout ourlet

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 console.log("Ce programme JS vient d'être chargé");
 $(document).ready(function() {
+    $('#ourlet').hide();
     console.log("Le document est pret");
     $('#formClient input').keyup(function(e) {
         console.log("touche appuyé");
         var texte = $(this).val();
         console.log(texte);
-        if ((/^[A-Z][a-zA-Z]+$/.test(texte) && texte.length < 20) || (/^[0-9]+\/[0-9]+\/2017$/.test(texte) && texte.length == 10))
+        if ((/^[a-zA-Z]+$/.test(texte) && texte.length < 20) || (/^[0-3][0-9]\/[0-1][0-9]\/2017$/.test(texte) && texte.length == 10))
             $(this).parent().find(".incoText").hide();
         else
             $(this).parent().find(".incoText").show();
@@ -16,21 +17,31 @@ $(document).ready(function() {
         if ($('#formClient span').is(":visible"))
             alert("Erreur! Certains champs ne sont pas complétés ou incorrect.");
         else {
-            console.log("test");
             var client = {
                 nom: $('#nom').val(),
                 prenom: $('#prenom').val(),
                 date: $('#date').val()
             };
             var nom = $('<tr><td class="nomP"></td></tr><tr><td class="prenomP"></td></tr><tr><td class="dateP"></td></tr>');
-            console.log($('#nom').val());
-            nom.find('.nomP').text("Nom du client : " + client.nom);
-            nom.find('.prenomP').text("Prénom du client : " + client.prenom);
+            nom.find('.nomP').text("Nom du client : " + client.nom.toUpperCase());
+            nom.find('.prenomP').text("Prénom du client : " + client.prenom.toUpperCase());
             nom.find('.dateP').text("Date de dépôt : " + client.date);
             $('#recap').append(nom);
         }
     });
 
+    $('#boutonOurlet').click(function(e) {
+        console.log("Bouton ourlet cliqué");
+        if ($('#boutonOurlet').text().localeCompare("+ Ourlet") === 0) {
+            console.log("Clique bouton plus");
+            $('#ourlet').show();
+            $('#boutonOurlet').text($('#boutonOurlet').text().replace('+ Ourlet', '- Ourlet'));
+        } else {
+            console.log("Clique bouton moins");
+            $('#ourlet').hide();
+            $('#boutonOurlet').text($('#boutonOurlet').text().replace('- Ourlet', '+ Ourlet'));
+        }
+    });
 
     console.log("La mise en place est finie. En attente d'événements...");
 });
